184f0be68519dd92a07e8a1feae13284ae42df3d
PaulKnees <jakewallace368@gmail.com>
2023-02-25 15:12:32 +0000

    projects. I hate it.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 // Components
 import Header from './components/Header'
 import ProjectsPage from './pages/ProjectsPage/ProjectsPage'
-import Contact from './components/Contact';
-import CVpage from './pages/CVpage'
+import ContactPage from './pages/ContactPage/ContactPage';
+import CVPage from './pages/CVPage/CVpage';
 // Pages
 import Hero from './components/Hero/Hero'
 // import { Form } from 'react-router-dom';
@@ -15,13 +15,13 @@ function App() {
     <div className="App">
       <Header />
 
-<Routes>
-  <Route path='/' element={<Hero/>} />
-  <Route path='/projects' element={<ProjectsPage/>} />
-  <Route path='/contact' element={<Contact/>} />
-  <Route path='CV' element={<CVpage />} />
+      <Routes style={{height:'100%'}}>
+        <Route path='/' element={<Hero />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='CV' element={<CVPage />} />
 
-</Routes>
+      </Routes>
     </div>
   );
 }
