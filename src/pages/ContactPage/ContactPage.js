850f2a56817982ac9de0d16e40e62b85e61259ec
PaulKnees <jakewallace368@gmail.com>
2023-02-25 23:20:30 +0000

    basic layout of Contact done

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -4,26 +4,26 @@ function Contact() {
     return (
         <section className="page contactContainer">
 
-            <div className='formHolder'>
-                <div className="contactForm">
-                    {/* <h1 className="lightText">Contact Me</h1> */}
-                    <form className="col">
-                        <input type='text' placeholder='Enter Your Name' />
-                        <input type='text' placeholder='Enter Your Email Address' />
-                        <textarea placeholder='Enter Message' cols='30' rows='10' />
-                    </form>
-                    
-                </div>
+
+            <div className="contactForm">
+                <h1 className="lightText">Contact Me</h1>
+                <form className="col">
+                    <input type='text' placeholder='Enter Your Name' />
+                    <input type='text' placeholder='Enter Your Email Address' />
+                    <textarea placeholder='Enter Message' cols='30' rows='10' />
+                </form>
             </div>
 
-            <div className='shapes'>
-            <div className='list'>
-                <ul>
-                    <li className="contact lightText"><b className="lightText">Email: </b>jakewallace368@gmail.com</li>
-                    <li className="contact lightText"><a href="https://github.com/Jake-W95" ><b className="lightText">GitHub</b></a></li>
-                    <li className="contact lightText"><a href="https://www.linkedin.com/in/jake-wallace-b2b85a25a/"><b className="lightText">Linkedin</b></a></li>
-                </ul>
-            </div>
+            <div className='shapesAndList'>
+                <div className='shapes'>
+                </div>
+                <div className='list'>
+                    <ul>
+                        <li className="contact lightText"><b className="lightText">Email: </b>jakewallace368@gmail.com</li>
+                        <li className="contact lightText"><a href="https://github.com/Jake-W95" ><b className="lightText">GitHub</b></a></li>
+                        <li className="contact lightText"><a href="https://www.linkedin.com/in/jake-wallace-b2b85a25a/"><b className="lightText">Linkedin</b></a></li>
+                    </ul>
+                </div>
             </div>
 
         </section>
