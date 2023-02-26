397553b215c0e5d54587bcfbc00f023d9867b546
PaulKnees <jakewallace368@gmail.com>
2023-02-26 00:04:47 +0000

    stripped down Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,21 +3,20 @@ function Hero() {
     return (
         <>
             <section className='heroSection'>
-                <div className='heroImage'></div>
-                <div className="heroTextBox"> 
-                <div className='heroHeaders'>
-                    <h1>
-                        I'm Jake <br></br>
-                        I'm a Web Developer</h1>
-                    <h4>
-                        Welcome to my portfolio,
-                        take a look at my work
-                    </h4>
+                <div className="heroTextBox">
+                    <div className='heroHeaders'>
+                        <h1 style={{
+                            fontSize: '5rem',
+                            color: 'white'
+                        }}>
+                            Wallace Development
+                        </h1>
+               
+
+                    </div>
 
                 </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </div> 
+                <div className='heroImage'></div>
             </section>
         </>
     )
